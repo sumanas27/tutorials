6c6d9d47435b529daceb6c6fa754c617557f9e65
Sumana Saha <sumana.saha@ratepay.com>
2020-05-29 10:45:43 +0200

    AccountService class implementation

---
+++ AccountService.java
@@ -0,0 +1,18 @@
+import java.utill.*;
+
+
+
+public class AccountService{
+	
+	private Account createAccount( AccountType accountType, String accountId ){
+		.....
+	}
+
+	.
+	.
+	.
+	.
+	.
+	.
+
+}
